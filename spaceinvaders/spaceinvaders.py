0a005a874b453e647fc014e2ea4b32ecad6c8a86
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-07-22 14:55:21 -0700

    Game over working

--- spaceinvaders/spaceinvaders.py
+++ spaceinvaders/spaceinvaders.py
@@ -1,29 +1,49 @@
 
 import pgzrun
 from random import random
+game_over=False
+game_start=False
 player=Actor("spaceship")
 player.x=400
 player.y=600-player.height *0.5
 lasers=[]
+cooldown=0
+cooled=True
+def cool():
+    global cooled
+    cooled=True
 aliens=[]
 def add_alien():
     alien=Actor('alien1')
     alien.x=alien.width*0.5+(800-alien.width)*random()
     alien.y=alien.height*0.5
     aliens.append(alien)
-clock.schedule_interval(add_alien,10)
+def on_key_down(key):
+    global game_start
+    game_start=True
+    clock.schedule_interval(add_alien,10)
 def fire_laser():
-    laser=Rect((player.x, player.y), (1,5))
-    lasers.append(laser)
+    global cooled, cooldown
+    if cooled:
+        laser=Rect((player.x, player.y), (1,50))
+        lasers.append(laser)
+        cooled=False
+        clock.schedule_unique(cool, cooldown)
 def update():
-    if keyboard.left and player.x>player.width*0.5:
-        player.x-=5
-    if keyboard.right and player.x<800-player.width*0.5:
-        player.x+=5
-    if keyboard.space:
-        fire_laser()
+    global game_over, game_start
+    if not game_start:
+        return
+    if not game_over:
+        if keyboard.left and player.x>player.width*0.5:
+            player.x-=5
+        if keyboard.right and player.x<800-player.width*0.5:
+            player.x+=5
+        if keyboard.space:
+            fire_laser()
     for alien in aliens:
-        alien.y+=1
+        alien.y+=3
+        if alien.y>600:
+            game_over=True
     for laser in lasers:
         laser.y-=5  
         for alien in aliens:
@@ -39,6 +59,8 @@ def draw():
         screen.draw.filled_rect(laser,(0, 255, 0))
     for alien in aliens:
         alien.draw()
+    if game_over:
+       screen.draw.text ("Game Over",(400, 300))
 
 pgzrun.go()
 
